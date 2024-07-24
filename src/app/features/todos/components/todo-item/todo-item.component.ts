ccc3eaec13bf6f325427727c7515f0199e49c05d
Nikolay Monov <n.monov@viv-design.com>
2024-07-24 07:19:57 +0300

    Todos - foreach todos - todo item styles - bootstrap icons

--- src/app/features/todos/components/todo-item/todo-item.component.ts
+++ src/app/features/todos/components/todo-item/todo-item.component.ts
@@ -1,17 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Todo } from '../../models/Todo';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.css'
 })
-export class TodoItemComponent implements OnInit {
+export class TodoItemComponent {
   @Input() todo!: Todo;
+  isOpened = false;
 
-  ngOnInit(): void {
-      console.log(this.todo);
+  toggleTodoContent() {
+    this.isOpened = !this.isOpened;
+  }
+
+  toggleIsCompleted() {
+    this.todo.isCompleted = !this.todo.isCompleted;
   }
 }
